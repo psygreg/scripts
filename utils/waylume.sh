77a94242267e4616045c26f060720de85e1e8fa9
psygreg <psygreg@icloud.com>
2026-04-04 23:54:49 -0300

    add waylume

---
+++ utils/waylume.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# name: WayLume
+# description: waylume_desc
+# icon: waylume.svg
+# nocontainer
+# desktop: gnome
+# repo: https://github.com/andrecavalcantebr/waylume
+
+curl -fsSL https://raw.githubusercontent.com/andrecavalcantebr/waylume/main/waylume.sh | bash -s -- --install
