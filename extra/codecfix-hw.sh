87309fa469031a5e9cce175a181de6d7352846b0
Victor <psygreg@icloud.com>
2026-04-08 06:34:40 -0300

    fix more robust checking for rpmfusion

--- extra/codecfix-hw.sh
+++ extra/codecfix-hw.sh
@@ -11,21 +11,22 @@
 source "$SCRIPT_DIR/libs/helpers.lib"
 _lang_
 sudo_rq
-rpmfusion_chk
-if command -v rpm-ostree &> /dev/null; then
-    sudo rpm-ostree override remove \
-    ffmpeg-free \
-    libavcodec-free \
-    libavfilter-free \
-    libavformat-free \
-    libavutil-free \
-    libpostproc-free \
-    libswresample-free \
-    libswscale-free \
-    libavdevice-free \
-    noopenh264 \
-    --install ffmpeg openh264 gstreamer1-plugin-openh264 libavcodec-freeworld mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld mesa-vulkan-drivers-freeworld libavcodec-freeworld gstreamer1-plugins-bad-freeworld
-else
-    sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+if rpmfusion_chk; then
+    if command -v rpm-ostree &> /dev/null; then
+        sudo rpm-ostree override remove \
+        ffmpeg-free \
+        libavcodec-free \
+        libavfilter-free \
+        libavformat-free \
+        libavutil-free \
+        libpostproc-free \
+        libswresample-free \
+        libswscale-free \
+        libavdevice-free \
+        noopenh264 \
+        --install ffmpeg openh264 gstreamer1-plugin-openh264 libavcodec-freeworld mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld mesa-vulkan-drivers-freeworld libavcodec-freeworld gstreamer1-plugins-bad-freeworld
+    else
+        sudo dnf swap ffmpeg-free ffmpeg --allowerasing
+    fi
 fi
 zeninf "$msg036"
