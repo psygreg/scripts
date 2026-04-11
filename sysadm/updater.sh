346c73cc7910ef47aea0321a1e2cb33033fe0552
psygreg <psygreg@icloud.com>
2026-04-11 19:39:41 -0300

    fix updater

--- sysadm/updater.sh
+++ sysadm/updater.sh
@@ -6,14 +6,19 @@
 
 source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
+sudo_rq
 
 prep_tmp
 wget https://raw.githubusercontent.com/psygreg/linuxtoys/master/resources/sysup/linuxtoys-update.service
 wget https://raw.githubusercontent.com/psygreg/linuxtoys/master/resources/sysup/linuxtoys-update.timer
+wget https://raw.githubusercontent.com/psygreg/linuxtoys/master/resources/sysup/linuxtoys-update-user.service
 prep_create /etc/systemd/system/linuxtoys-update.service
 prep_create /etc/systemd/system/linuxtoys-update.timer
+prep_create /etc/systemd/system/linuxtoys-update-user.service
+sed -i "s/@USER@/$USER/g" linuxtoys-update-user.service
 copy_ -f linuxtoys-update.service /etc/systemd/system/linuxtoys-update.service
 copy_ -f linuxtoys-update.timer /etc/systemd/system/linuxtoys-update.timer
+copy_ -f linuxtoys-update-user.service /etc/systemd/system/linuxtoys-update-user.service
 sudo systemctl daemon-reload
 sysd_enable linuxtoys-update.timer
 sysd_start linuxtoys-update.timer
