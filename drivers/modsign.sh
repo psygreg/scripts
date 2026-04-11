d83e8ea0d07fd0e1d436266af9bca0477a338ee4
psygreg <psygreg@icloud.com>
2026-04-11 20:13:48 -0300

    add fedora compat to modsigning

--- drivers/modsign.sh
+++ drivers/modsign.sh
@@ -3,7 +3,7 @@
 # version: 1.0
 # description: modsign_desc
 # icon: sign.svg
-# compat: ostree
+# compat: ostree, fedora
 # reboot: yes
 # nocontainer
 # repo: https://github.com/CheariX/silverblue-akmods-keys
@@ -13,15 +13,21 @@ source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
 sudo_rq
 if sudo mokutil --sb-state | grep -q "SecureBoot enabled"; then
-    if ! rpm -qi "akmods-keys" &>/dev/null; then
-        pkg_install rpmdevtools akmods
-        sudo kmodgenca
-        sudo mokutil --import /etc/pki/akmods/certs/public_key.der
-        prep_tmp
-        git clone https://github.com/CheariX/silverblue-akmods-keys
-        cd silverblue-akmods-keys
-        sudo bash setup.sh
-        pkg_fromfile akmods-keys-0.0.2-8.fc$(rpm -E %fedora).noarch.rpm
+    if is_ostree; then
+        if ! rpm -qi "akmods-keys" &>/dev/null; then
+            pkg_install rpmdevtools akmods
+            sudo kmodgenca
+            sudo mokutil --import /etc/pki/akmods/certs/public_key.der
+            prep_tmp
+            git clone https://github.com/CheariX/silverblue-akmods-keys
+            cd silverblue-akmods-keys
+            sudo bash setup.sh
+            pkg_fromfile akmods-keys-0.0.2-8.fc$(rpm -E %fedora).noarch.rpm
+        fi
+    elif is_fedora; then
+        pkg_install kmodtool akmods mokutil openssl
+        sudo kmodgenca -a
+        sudo mokutil --import /etc/pki/akmods/certs/public_key.der # displays enroll MOK prompt on reboot
     fi
 fi
 zeninf "$msg036"
