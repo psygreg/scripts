f9817267a04cc20d20646b31a16cd8725ae9e8ae
psygreg <psygreg@icloud.com>
2026-05-13 12:29:13 -0300

    fix linuxtoys/#492

--- devs/ides/code.sh
+++ devs/ides/code.sh
@@ -24,7 +24,7 @@ elif is_arch || is_cachy; then
     pkg_install visual-studio-code-bin
     zeninf "$msg018"
     exit 0
-elif is_fedora; then
+elif is_fedora || is_ostree; then
     if command -v rpm-ostree &>/dev/null; then
         echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
     else
