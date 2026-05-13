c5612524319699873a258d3afee686e06e15002b
psygreg <psygreg@icloud.com>
2026-05-13 12:26:37 -0300

    fix linuxtoys/#490

--- game/gfn.sh
+++ game/gfn.sh
@@ -9,7 +9,8 @@
 source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
 pkg_flat org.freedesktop.Sdk//24.08
-flatpak remote-add --system --if-not-exists GeForceNOW https://international.download.nvidia.com/GFNLinux/flatpak/geforcenow.flatpakrepo
-flatpak install --or-update --system --noninteractive GeForceNOW com.nvidia.geforcenow
+sudo_rq
+{ flatpak remote-add --system --if-not-exists GeForceNOW https://international.download.nvidia.com/GFNLinux/flatpak/geforcenow.flatpakrepo || sudo flatpak remote-add --system --if-not-exists GeForceNOW https://international.download.nvidia.com/GFNLinux/flatpak/geforcenow.flatpakrepo; } || fatal "Failed to add GeForce NOW Flatpak repository."
+{ flatpak install --or-update --system --noninteractive GeForceNOW com.nvidia.geforcenow || sudo flatpak install --or-update --system --noninteractive GeForceNOW com.nvidia.geforcenow; } || fatal "Failed to install GeForce NOW Flatpak."
 _append_transmap "flatpak com.nvidia.geforcenow"
-flatpak override --system --nosocket=wayland com.nvidia.geforcenow
+flatpak override --system --nosocket=wayland com.nvidia.geforcenow || sudo flatpak override --system --nosocket=wayland com.nvidia.geforcenow
