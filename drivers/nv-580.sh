9a7b171fe2ea67dcf9a333326411ebb24b9dc56b
Victor Gregory 'psygreg <psygreg@icloud.com>
2026-05-30 11:37:35 -0300

    make nvidia v580 driver installer available for Fedora/RHEL-like systems

--- drivers/nv-580.sh
+++ drivers/nv-580.sh
@@ -4,7 +4,7 @@
 # icon: nvidia.svg
 # nocontainer
 # gpu: Nvidia
-# compat: debian, arch, cachy
+# compat: debian, arch, cachy, fedora, rhel
 # reboot: yes
 
 # --- Start of the script code ---
@@ -12,11 +12,11 @@ source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
 sudo_rq
 if is_debian; then
-    local debian_ver=$(lsb_release -rs 2>/dev/null)
+    debian_ver=$(lsb_release -rs 2>/dev/null)
     prep_tmp
     pkg_install gcc lsb-release software-properties-common
     sudo add-apt-repository contrib
-    wget "https://developer.download.nvidia.com/compute/cuda/repos/debian$debian_ver/x86_64/cuda-keyring_1.1-1_all.deb"
+    wget "https://developer.download.nvidia.com/compute/cuda/repos/debian${debian_ver:-trixie}/x86_64/cuda-keyring_1.1-1_all.deb"
     pkg_fromfile cuda-keyring_1.1-1_all.deb
     sleep 1
     sudo apt update
@@ -27,6 +27,12 @@ if is_debian; then
     zeninf "$msg036"
 elif is_arch || is_cachy; then
     pkg_install nvidia-580xx-dkms nvidia-580xx-utils nvidia-580xx-settings
+    initramfs_upd
+    bootloader_upd
+    zeninf "$msg036"
+elif is_fedora || is_rhel; then
+    rpmfusion_chk
+    pkg_install akmod-nvidia-580xx xorg-x11-drv-nvidia-580xx-cuda
     initramfs_upd
     bootloader_upd
     zeninf "$msg036"
