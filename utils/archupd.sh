e32b128ae82e6dfa8d801e6d36eb19adea2ad034
Victor Gregory 'psygreg <psygreg@icloud.com>
2026-05-31 15:25:54 -0300

    fix linuxtoys/#591

--- utils/archupd.sh
+++ utils/archupd.sh
@@ -13,10 +13,9 @@ source "$SCRIPT_DIR/libs/linuxtoys.lib"
 _lang_
 sudo_rq
 pkg_install arch-update
-sysd_enable arch-update-tray.service
-sysd_start arch-update-tray.service
-sysd_enable arch-update.timer
-sysd_start arch-update.timer
+sleep 2
+{ sysd_enable arch-update-tray.service 2>/dev/null && sysd_start arch-update-tray.service 2>/dev/null; } || arch-update --tray --enable
+sysd_enable arch-update.timer || fatal "Failed to enable arch-update timer."
+sysd_start arch-update.timer || zenwrn "Failed to start but succesfully enabled arch-update timer. It will start on the next boot."
 sleep 1
-arch-update --tray --enable
 zeninf "$msg018"
